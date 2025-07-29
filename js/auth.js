56bae55473480968e9b086c601feaae9a98905de
Irfandi Riawan <irfandi.riawan@yahoo.com>
2025-07-29 13:26:51 +0700

    Change to block rendering until verification

--- js/auth.js
+++ js/auth.js
@@ -21,7 +21,7 @@
         }
 
         // Token is valid; do nothing and allow the page to load
-
+        document.body.classList.remove('hidden');
     } catch (error) {
         console.error('Authentication check failed:', error);
         window.location.href = '/login.html';
